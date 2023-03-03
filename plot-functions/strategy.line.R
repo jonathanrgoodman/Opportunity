1dcdc214aa88965e6ef19d3f18d515e448c74ca6
Jonathan R Goodman <98103956+jonathanrgoodman@users.noreply.github.com>
2023-03-03 14:10:46 +0000

    Update strategy.line.R

--- plot-functions/strategy.line.R
+++ plot-functions/strategy.line.R
@@ -1,8 +1,17 @@
-PD.frequency <- function(data){
+PD.frequency <- function(data, every = 100){
   
   require(ggplot2)
-  ggplot(data=data, aes(x=generation,y=Freq, color=strategy))+
+  
+  #subset generations by every; default = 100
+  
+ coop <- seq(1,nrow(data),every)
+ def <- seq(2,nrow(data),every)
+ 
+ data.every <- c(coop,def)
+ data.every <- sort(data.every)
+  
+  ggplot(data=data[data.every,], aes(x=generation,y=Freq, color=strategy))+
   geom_line()+
-  scale_color_manual(values=c("green","red"), labels=c("cooperation","defection"))+labs(y="frequency")+theme_bw()
+  scale_color_manual(values=c("green","red"), labels=c("cooperator","defector"))+labs(y="frequency")+theme_bw()
   
 }
