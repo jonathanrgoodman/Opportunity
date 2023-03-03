948630187fa72e93194e3989eda0fb3a0c2b80b6
Jonathan R Goodman <98103956+jonathanrgoodman@users.noreply.github.com>
2023-03-03 14:11:20 +0000

    Update cooperation.line.R

--- plot-functions/cooperation.line.R
+++ plot-functions/cooperation.line.R
@@ -1,7 +1,12 @@
-coop.frequency <- function(data){
+coop.frequency <- function(data, every = 100){
   
   require(ggplot2)
-  ggplot(data=data, aes(x=generation,y=cooperation))+
+  
+  #subset generations by every; default = 100
+  
+  coop <- seq(1,nrow(data),every)
+
+  ggplot(data=data[coop,], aes(x=generation,y=cooperation))+
   geom_line()+
   ylim(0, 100)+
   theme_bw()
