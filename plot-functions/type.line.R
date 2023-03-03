a8335bd9dba29d4671083101dd6df2e6c10a25c9
Jonathan R Goodman <98103956+jonathanrgoodman@users.noreply.github.com>
2023-03-03 14:11:04 +0000

    Update type.line.R

--- plot-functions/type.line.R
+++ plot-functions/type.line.R
@@ -1,7 +1,16 @@
-type.line <- function(data){
+type.line <- function(data, every = 100){
   
   require(ggplot2)
-  ggplot(data=data, aes(x=generation,y=Freq, color=type))+
+  
+  #subset generations by every; default = 100
+  
+  honest <- seq(1,nrow(data),every)
+  mimic <- seq(2,nrow(data),every)
+  
+  data.every <- c(honest,mimic)
+  data.every <- sort(data.every)
+  
+  ggplot(data=data[data.every,], aes(x=generation,y=Freq, color=type))+
     geom_line()+
     scale_color_manual(values=c("blue","orange"), labels=c("honest","mimic"))+labs(y="frequency")+theme_bw()
   
